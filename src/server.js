e9518b05a74a3f24097187d91fdcbbd1dddb6e57
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-05-05 17:31:39 -0300

    🖥️ Configurando ambiente 👍

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import app from './App.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`CRTL + Click to open in browser: http://localhost:${PORT}`);
+    
+})
