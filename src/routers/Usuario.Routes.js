137003858b9da24411582cca5fbcf715757e2ece
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-05-26 16:54:10 -0300

    crud do usuario😎🥶

--- src/routers/Usuario.Routes.js
+++ src/routers/Usuario.Routes.js
@@ -5,5 +5,8 @@ const routerUsuario = Router()
 
 routerUsuario.get('/', UsuarioController.index)
 routerUsuario.post('/', UsuarioController.store)
+routerUsuario.get('/:email', UsuarioController.show)
+routerUsuario.put('/:email', UsuarioController.update)
+routerUsuario.delete('/id', UsuarioController.delete)
 
 export default routerUsuario
